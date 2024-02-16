7364d78fd545bc76fb3ac0cfdfa4840d9b0bcc51
Steve Steinitz <steve@stzdev.com>
2024-02-16 18:01:21 +1100

    add fund button, get it working

--- index.js
+++ index.js
@@ -1,21 +1,59 @@
+import {ethers} from "https://cdnjs.cloudflare.com/ajax/libs/ethers/6.7.0/ethers.min.js"
+import {abi, contractAddress} from "./constants.js"
+
+// console.log({abi});
+
+const connectButton = document.getElementById("connectButton")
+const fundButton = document.getElementById("fundButton")
+connectButton.onclick = connect
+fundButton.onclick = fund
+
+const getEthWallet = () => window.ethereum
+
+const isEthWalletInstalled = () => {
+  return typeof getEthWallet() !== "undefined"
+}
+
 async function connect() {
-  if(typeof(window.ethereum) !== undefined) {
-    const ethereum = window.ethereum
-    if (ethereum.isMetaMask) {
+  if(isEthWalletInstalled()) {
+    if (getEthWallet().isMetaMask) {
       console.log('MetaMask, installed -- connecting')
     }
     else {
       console.log('Ethereum wallet installed -- connecting')
     }
-    document.getElementById("connectButton").innerHTML = "Connecting..."
-    await window.ethereum.request(
-      {method: 'eth_requestAccounts'}
-    )
-    document.getElementById("connectButton").innerHTML = "Connected"
+    connectButton.innerHTML = "Connecting..."
+    try {
+      await getEthWallet().request(
+        {method: 'eth_requestAccounts'}
+      )}
+    catch (e) {
+      console.log('caught error', {e})
+    }
+    connectButton.innerHTML = "Connected"
     console.log('Connected')
   }
   else {
-    document.getElementById("connectButton").innerHTML = "Please install Metamask"
+    connectButton.innerHTML = "Please install Metamask"
     console.log('no ethereum wallet found')
   }
 }
+
+// fund 
+// async function fund(ethAmount) {
+async function fund() {
+  const ethAmount = '7' // temporary until we pass the value as an argument or...
+  console.log(`Funding with ${ethAmount}`)
+  if(isEthWalletInstalled()) {
+    // get a connection to the blockchain
+    const provider = new ethers.BrowserProvider(getEthWallet())
+
+    const signer = await provider.getSigner()
+    const contract = new ethers.Contract(contractAddress, abi, signer)
+    /* const transactionRespnse = */ await contract.fund(
+      {value: ethers.parseEther(ethAmount)}
+    )
+  }
+}
+
+// withdraw
